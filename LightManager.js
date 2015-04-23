b94baf67b237958370726d63d97b02c15a74e29d
Victor Garrido <v.garrido@mediamonks.com>
2015-04-23 17:19:28 +0200

    First deploy

---
+++ LightManager.js
@@ -0,0 +1,77 @@
+var LightManager =
+{
+	create: function( options )
+	{
+		var self = Object.create( this );
+		self.options = options;
+		self._update = self.update.bind( self );
+		self.then = 0;
+		self.fpsInterval = 1000 / self.options.fps;
+		self.rafId = 0;
+
+		self.children = [];
+		self.options.stage.canvas.width = self.options.stage.width;
+		self.options.stage.canvas.height = self.options.stage.height;
+
+		self.ctx = self.options.stage.canvas.getContext('2d');
+
+		return self;
+	},
+
+	add: function( child )
+	{
+		this.children.push( child );
+	},
+
+	remove: function( child )
+	{
+		var index = this.children.indexOf(child);
+		if(index !== -1)
+		{
+			this.children.splice(index, 0);
+		}
+	},
+
+	setFPS: function( fps )
+	{
+		this.fpsInterval = 1000 / fps;
+	},
+
+	setScale: function(scale)
+	{
+		var length = this.children.length;
+		while(length--)
+		{
+			this.children[length].scale = scale;
+		}
+	},
+
+	start: function()
+	{
+		this.rafId = requestAnimationFrame( this._update );
+	},
+
+	stop: function()
+	{
+		cancelRequestAnimationFrame( this.rafId );
+	},
+
+	update: function( timestamp )
+	{
+		// calc elapsed time since last loop
+		var elapsed = timestamp - this.then;
+
+		// if enough time has elapsed, draw the next frame
+		if( elapsed > this.fpsInterval )
+		{
+			var length = this.children.length;
+			while(length--)
+			{
+				this.children[length].draw(timestamp, this.ctx);
+			}
+			this.then = timestamp - (elapsed % this.fpsInterval);
+		}
+		requestAnimationFrame( this._update );
+	}
+
+};
---
+++ LightSpritesheet.js
@@ -0,0 +1,63 @@
+var LightSpritesheet =
+{
+	FORWARD: 'direction.forward',
+
+	REVERSE: 'direction.reverse',
+
+	create: function( options )
+	{
+		var self = Object.create( this );
+		self.options = options;
+		self.currentSpriteProps = {iX: 0, iY: 0, x: 0, y: 0};
+		self.currentFrame = 0;
+
+		self.spritesheet = new Image();
+		self.spritesheet.onload = self.handleSpritesheetLoaded.bind( self );
+		self.spritesheet.src = self.options.frames.src;
+
+		self.direction = 1;
+		self.scale = 1;
+
+		return self;
+	},
+
+	init: function()
+	{
+
+	},
+
+	draw: function(timestamp, ctx)
+	{
+		if( (this.direction === 1 && this.currentFrame < this.options.frames.count - 1) ||
+			(this.direction === -1 && this.currentFrame > 0) )
+		{
+			this.currentFrame += this.direction;
+		}
+		else if(this.options.loop)
+		{
+			this.currentFrame = 0;
+			console.log('loop')
+		}
+
+		//Get spritesheet frame position
+		this.currentSpriteProps.iX = this.currentFrame % this.options.cols;
+		this.currentSpriteProps.iY = Math.floor( this.currentFrame / this.options.cols );
+		this.currentSpriteProps.x = this.currentSpriteProps.iX * this.options.frames.width;
+		this.currentSpriteProps.y = this.currentSpriteProps.iY * this.options.frames.height;
+
+		// Draw mask
+		//this.ctx.drawImage( this.spritesheet, this.currentSpriteProps.x, this.currentSpriteProps.y, this.options.frames.width, this.options.frames.height, this.options.x, this.options.y, this.options.frames.width * self.scale, this.options.frames.height*self.scale );
+		ctx.clearRect(0, 0, this.options.frames.width * this.scale, this.options.frames.height * this.scale );
+		ctx.drawImage( this.spritesheet, this.currentSpriteProps.x, this.currentSpriteProps.y, this.options.frames.width, this.options.frames.height, 0, 0, this.options.frames.width * this.scale, this.options.frames.height * this.scale );
+	},
+
+	setDirection: function( direction )
+	{
+		this.direction = direction === LightSpritesheet.FORWARD ? 1 : -1;
+	},
+
+	handleSpritesheetLoaded: function()
+	{
+
+	}
+}
